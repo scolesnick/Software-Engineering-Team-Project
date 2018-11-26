02f89bbba5662088adcc0764203aeb16a4122ef2
Tanner <Tanner@Tanner-PC.Home>
2018-11-25 20:28:46 -0600

    handles current messages from server for logging in and creating an account

--- BulletPOW/client/GameClient.java
+++ BulletPOW/client/GameClient.java
@@ -1,8 +1,11 @@
 package client;
 
+import client.CreateAccountControl;
+import client.LoginControl;
 import gameMechanics.Player;
+import ocsf.client.AbstractClient;
 
-public class GameClient
+public class GameClient extends AbstractClient
 {
 	private Player player;
 	private LoginControl loginCtrl;
@@ -10,9 +13,39 @@ public class GameClient
 	private JoinGameControl joinCtrl;
 	private GameControl gameCtrl;
 	
-	// Constructor
-	public GameClient()
-	{
-		
-	}
+  public GameClient()
+  {
+	  super("localhost",8300);
+  }
+  
+  public void setCreateController(CreateAccountControl cc) 
+  {
+	  createCtrl = cc;
+  }
+  
+  public void setLoginController(LoginControl lc) 
+  {
+	  loginCtrl = lc;
+  }
+
+  public void handleMessageFromServer(Object arg0)
+  {
+	  System.out.println("Server Message sent to Client " + (String)arg0);
+	  if (arg0.equals("true"))
+		  loginCtrl.loginSuccess();
+	  else if (arg0.equals("false"))
+		  loginCtrl.displayError("Invalid login credentials.");
+	  
+	  if (arg0.equals("valid"))
+		  createCtrl.createSuccess();
+	  else if (arg0.equals("duplicate"))
+		  createCtrl.displayError("Duplicate username. ");
+	  else if (arg0.equals("mathching"))
+		  createCtrl.displayError("Passwords don't match. ");
+	  else if (arg0.equals("longpass"))
+		  createCtrl.displayError("Password too long. ");
+	  else if (arg0.equals("shortpass"))
+		  createCtrl.displayError("Password too short. ");
+	  }
 }
+  
