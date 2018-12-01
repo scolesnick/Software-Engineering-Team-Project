52d01552f5923d9d2de72385e004bd4da15685fd
josh <josh@JoshOPC>
2018-12-01 12:34:08 -0600

    testing12

--- BulletPOW/client/GameMap.java
+++ BulletPOW/client/GameMap.java
@@ -1,94 +1,153 @@
 package client;
 
-import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
 
-public class GameMap extends JPanel implements ActionListener
-{
-	private ImageIcon image;
-	private Timer timer;
-	private int delay = 1000/60; //milliseconds
-	private int x=20, y=30;
-	private int player_speed = 5;
+public class GameMap extends JPanel implements ActionListener{
 	
-	private boolean up = false;
-	private boolean down = false;
-	private boolean left = false;
-	private boolean right = false;
+	private BufferedImage dude;
+	private Image dudeImage;
+	private int x=0, y=0, player_speed=5;
+	private Timer timer;
+	private boolean dPressed, aPressed, sPressed, wPressed;
 	
 	public GameMap() {
-//		image = new ImageIcon(this.getClass().getResource("/guy1.jgp"));
-		try
-		{
-			image = new ImageIcon(ImageIO.read(new File("../BulletPOW/guy1.jpg")));
-		} catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		addKeyBinding(this, false, KeyEvent.VK_W, "moveUp", (evt)->{up = true;});
-		addKeyBinding(this, false, KeyEvent.VK_S, "moveDown", (evt)->{down = true;});
-		addKeyBinding(this, false, KeyEvent.VK_A, "moveLeft", (evt)->{left = true;});
-		addKeyBinding(this, false, KeyEvent.VK_D, "moveRight", (evt)->{right = true;});
-		init();
-	}
-	private void addKeyBinding(JComponent comp, boolean release, int keyCode, String id, ActionListener actionListener) {
-		InputMap im = comp.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		im.put(KeyStroke.getKeyStroke(keyCode, 0, release), id);
-		ActionMap am = comp.getActionMap();
-		am.put(id, new AbstractAction() {			
+		//creates a buffered image from the jpg stored above the package
+		try {dude = ImageIO.read(new File("guy1.jpg"));} catch (IOException ex) {}
+		
+		setBackground(Color.BLACK);
+		
+		//creates an Image based on the BufferedImage that initially loads the file
+		dudeImage = dude.getScaledInstance(dude.getWidth(), dude.getHeight(), Image.SCALE_SMOOTH);
+		
+		//instantiates timer for refresh purposes - changing 60 to smaller number means slower, choppier movement
+		timer = new Timer (1000/60, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                move();
+                repaint();
+            }
+        });
+        timer.start();
+		
+		addKeyBindings();
+    }
+	
+	public void addKeyBindings() {
+		
+		//add possible keystrokes
+		InputMap im = this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false), "dPressed");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true), "dReleased");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, false), "aPressed");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, true), "aReleased");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, false), "sPressed");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, true), "sReleased");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, false), "wPressed");
+		im.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, true), "wReleased");
+		
+		//add keystroke actions
+		ActionMap ap = this.getActionMap();
+		//for button d
+		ap.put("dPressed", new AbstractAction() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				actionListener.actionPerformed(e);
+			public void actionPerformed(ActionEvent e) {
+				dPressed = true;
+			}
+		});
+		ap.put("dReleased", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dPressed = false;
+			}
+		});
+		
+		//for button w
+		ap.put("wPressed", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				wPressed = true;
+			}
+		});
+		ap.put("wReleased", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				wPressed = false;
+			}
+		});
+		
+		//for button a
+		ap.put("aPressed", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				aPressed = true;
+			}
+		});
+		ap.put("aReleased", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				aPressed = false;
+			}
+		});
+		
+		//for button s
+		ap.put("sPressed", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sPressed = true;
+			}
+		});
+		ap.put("sReleased", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sPressed = false;
 			}
 		});
 	}
 	
-	public void init() {
-		timer = new Timer(delay, this);
-		timer.setActionCommand("Timer");
-		timer.start();
+	public void move() {
+		//these if statements tell the image what to do when a certain key is pressed
+		if (dPressed) {
+			if (x+player_speed < getPanelWidth() - dude.getWidth()) {
+				x += player_speed;
+			}
+		}
+		if (aPressed) {
+			if(x-player_speed > 0) {
+				x -= player_speed;
+			}
+		}
+		if (sPressed) {
+			if(y+player_speed < this.getHeight() - dude.getHeight()) {
+				y += player_speed;
+			}
+		}
+		if (wPressed) {
+			if(y-player_speed > 0) {
+				y -= player_speed;
+			}
+		}
 	}
 	
-	public void paintComponent (Graphics page) {
-		   super.paintComponent (page);
-		   image.paintIcon (this, page, x, y);
-		}
 	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		String command = e.getActionCommand();
-		if(command == "Timer") {
-//			System.out.println("Timer ticked");
-			if(up) {
-				if(y-player_speed > 0) {
-					y -= player_speed;
-				}
-			}
-			if(down) {
-				if(y+player_speed < this.getHeight() - image.getIconHeight()) {
-					y += player_speed;
-				}
-			}
-			if(left) {
-				if(x-player_speed > 0) {
-					x -= player_speed;
-				}
-			}
-			if(right) {
-				if(x+player_speed < this.getWidth() - image.getIconWidth()) {
-					x += player_speed;
-				}
-			}
-			up=down=left=right=false;
-			repaint();
-		}
+    protected void paintComponent(Graphics g) {
+		//paints the player image on the panel
+        super.paintComponent(g);
+        g.drawImage(dudeImage, x, y, this);         
+    }
+	
+	public int getPanelWidth() {
+		return this.getWidth();
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {}
 }
