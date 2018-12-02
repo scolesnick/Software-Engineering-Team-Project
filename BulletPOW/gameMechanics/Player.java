fdab6ef630922ff25e174826c21e432a1c2b4e76
numbscole <scolesnick@gmail.com>
2018-12-01 20:47:38 -0600

    changed package position

--- BulletPOW/gameMechanics/Player.java
+++ BulletPOW/gameMechanics/Player.java
@@ -1,23 +1,73 @@
 package gameMechanics;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Player {
+public class Player implements Serializable{
 	
 	private messageData.LoginData data;
-	private int health;
-	private int speed;
-	private ArrayList<Bullets> bullets;
+	private int health = 5;
+	private int speed = 5;
+	private int x = 0, y = 0;
+	//private ArrayList<Bullets> bullets;
 
-	public void Player() {
-		
-		
-		
+	public int getX()
+	{
+		return x;
 	}
 	
-	public void updateStats() {
-		
-		
+	public int getY()
+	{
+		return y;
+	}
+	
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+	
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+	
+	public int getHealth()
+	{
+		return health;
+	}
+	
+	public void setHealth(int h)
+	{
+		health = h;
+	}
+	
+	public void setSpeed(int s)
+	{
+		speed = s;
+	}
+	
+	public int getSpeed()
+	{
+		return speed;
+	}
+	
+	/*public ArrayList<Bullets> getBullets()
+	{
+		return bullets;
+	}*/
+	
+	/*public void addBullet(Bullets b)
+	{
+		bullets.add(b);
+	}*/
+	
+	public void updateHealth(int h)
+	{
+		this.health += h;
+	}
+	
+	public Player()
+	{
 		
 	}
 }
