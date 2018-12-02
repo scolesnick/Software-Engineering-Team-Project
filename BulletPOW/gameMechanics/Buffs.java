718be3784d745467b7ee6bdf351b161e42e2a132
senclairv <43869557+senclairv@users.noreply.github.com>
2018-12-01 21:33:42 -0600

    Add files via upload

--- BulletPOW/gameMechanics/Buffs.java
+++ BulletPOW/gameMechanics/Buffs.java
@@ -6,5 +6,44 @@ public class Buffs {
 	private int gainHealth;
 	private int speedMult;
 	private int duration;
-
+	
+	public void setDamage(int d)
+	{
+		damage = d;
+	}
+	
+	public void setGainHealth(int g)
+	{
+		gainHealth = g;
+	}
+	
+	public void speedMult(int s)
+	{
+		speedMult = s;
+	}
+	
+	public void setDuration(int d)
+	{
+		duration = d;
+	}
+	
+	public int getDamage()
+	{
+		return damage;
+	}
+	
+	public int getGainHealth()
+	{
+		return gainHealth;
+	}
+	
+	public int getSpeedMult()
+	{
+		return speedMult;
+	}
+	
+	public int getDuration()
+	{
+		return duration;
+	}
 }
