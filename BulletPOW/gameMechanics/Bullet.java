044c16c50b1ab09f0beba2f7255cbe0d63b3305f
numbscole <scolesnick@gmail.com>
2018-12-04 15:37:04 -0600

    Unit Test for Bullet

--- BulletPOW/gameMechanics/Bullet.java
+++ BulletPOW/gameMechanics/Bullet.java
@@ -9,8 +9,6 @@ public class Bullet
 	private int x;
 	private int y;
 	private double angle, xVel=0, yVel=0;
-	
-//	private Rectangle box;
 	private int bullet_speed=10;
 	
 	public Bullet(int x, int y) 
@@ -22,32 +20,6 @@ public class Bullet
 		
 	}
 
-//	public void setFly(boolean f)
-//	{
-//		flying = f;
-//	}
-//	public boolean getFly()
-//	{
-//		return flying;
-//	}
-//	public void setBox() 
-//	{
-//		box = new Rectangle(x,y,b_width,b_height);
-//	}
-//	public void resetBox() 
-//	{
-//		box = new Rectangle(0,0,0,0);
-//		System.out.println("box reset");
-//		box = null;
-//		x = y = 0;
-//		xVel = yVel = 0;
-//		flying = false;
-//	}
-//	public Rectangle getBox() 
-//	{
-//		return box;
-//	}
-	
 	public void setVelocity(int mouseY, int mouseX) 
 	{
 		angle = Math.atan2(mouseY-y, mouseX-x);
@@ -102,30 +74,6 @@ public class Bullet
 	
 	public int nextX() {return this.x + (int)xVel;}
 	public int nextY() {return this.y + (int)yVel;}
-	public void step() {this.y += yVel; this.x += xVel;}
-	
-//	private void checkBounds(int xMin, int yMin, int xMax, int yMax)
-//	{
-//		if (box.x <= xMin || box.x >= xMax)
-//		{
-//			resetBox();
-//		}
-//		else if (box.y <= yMin || box.x >= yMax)
-//		{
-//			resetBox();
-//		}
-//	}
-//	public int checkCollision(int opponentX, int opponentY)
-//	{
-//		if(box != null)
-//			if (Math.abs(box.x - opponentX) < 20 && Math.abs(box.y - opponentY) < 15)
-//			{
-//				System.out.println("collision");
-//				resetBox();
-//				return -damage;
-//			}
-//		return 0;
-//	}
-	
+	public void step() {this.y += yVel; this.x += xVel;}	
 
 }
